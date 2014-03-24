6ca9a5b534ea68e73e99260a2929bdc53d73929f
a-tsioh <pmagistry@gmail.com>
2014-03-24 22:52:59 +0800

    +data fetcher and data

---
+++ fetch_data.py
@@ -0,0 +1,23 @@
+import json
+import urllib2
+import codecs
+
+channels = ["fumao.word.live", "fumao.tw"]
+dates = ["2014-03-2" + str(x) for x in range(4)]
+
+base_url = "http://logbot.g0v.tw/channel/%s/%s/json"
+
+data = []
+
+for chan in channels:
+    for date in dates:
+        url = base_url % (chan, date)
+        json_data = urllib2.urlopen(url).read()
+        data.extend(json.loads(json_data))
+
+
+outfile = codecs.open("Data/all.json", "w", "utf8")
+json.dump(data, outfile, ensure_ascii=False, encoding='utf-8')
+outfile.close()
+
+
